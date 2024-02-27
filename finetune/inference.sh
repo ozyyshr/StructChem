c7d01137f0f79d209dafdd72ee021438f7303fbd
Siru Ouyang <siruo2@dmserv21.cs.illinois.edu>
2024-02-27 17:51:15 -0600

    updated

--- finetune/inference.sh
+++
@@ -1,3 +0,0 @@
-python inference.py \
-    --base_model '/shared/data3/checkpoints/llama-2-13b-chat-hf' \
-    --lora_weights '/shared/data3/siruo2/instruct_chem/lora-llama2-13bchat-1k/checkpoint-1900'
